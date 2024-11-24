47be24d08e83f76eb608b3bbd656de2e59fbd68d
hellonayeon <hellonykwon@gmail.com>
2024-11-24 18:59:04 +0900

    feat: EAGER, LAZY 전략에 따른 분기 처리

--- src/main/java/persistence/entity/loader/DefaultEntityLoader.java
+++ src/main/java/persistence/entity/loader/DefaultEntityLoader.java
@@ -1,7 +1,12 @@
 package persistence.entity.loader;
 
+import jakarta.persistence.FetchType;
+import java.lang.reflect.Field;
 import java.util.List;
 import jdbc.JdbcTemplate;
+import persistence.entity.loader.proxy.CollectionEntityProxy;
+import persistence.meta.ColumnMeta;
+import persistence.meta.RelationMeta;
 import persistence.meta.SchemaMeta;
 import persistence.sql.dml.query.WhereCondition;
 import persistence.sql.dml.query.WhereOperator;
@@ -31,9 +36,35 @@ public class DefaultEntityLoader implements EntityLoader {
             return instance;
         }
 
-        return collectionLoader.loadCollection(clazz, instance);
+        List<ColumnMeta> columnMetasWithRelation = schemaMeta.columnMetasHasRelation();
+        for (ColumnMeta columnMeta : columnMetasWithRelation) {
+            RelationMeta relationMeta = columnMeta.relationMeta();
+            if (FetchType.EAGER == relationMeta.fetchType()) {
+                List<?> children = eagerLoad(columnMeta, id);
+                mapChildrenField(instance, columnMeta, children);
+            }
+
+            if (FetchType.LAZY == relationMeta.fetchType()) {
+                List<?> children = CollectionEntityProxy.createProxy(columnMeta, id, collectionLoader);
+                mapChildrenField(instance, columnMeta, children);
+            }
+        }
+
+        return clazz.cast(instance);
     }
 
+    private List<?> eagerLoad(ColumnMeta columnMeta, Object id) {
+        return collectionLoader.load(columnMeta.relationMeta().joinColumnType(), columnMeta.field(), id);
+    }
 
+    private void mapChildrenField(Object instance, ColumnMeta columnMeta, List<?> children) {
+        try {
+            Field field = columnMeta.field();
+            field.setAccessible(true);
+            field.set(instance, children);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/main/java/persistence/entity/loader/proxy/CollectionEntityProxy.java
+++ src/main/java/persistence/entity/loader/proxy/CollectionEntityProxy.java
@@ -7,11 +7,11 @@ import java.util.List;
 import persistence.entity.loader.EntityCollectionLoader;
 import persistence.meta.ColumnMeta;
 
-public class CollectionEntityProxy implements InvocationHandler {
+public class CollectionEntityProxy<T> implements InvocationHandler {
 
     private final ColumnMeta columnMeta;
     private final Object id;
-    private List<?> target;
+    private List<T> target;
     private final EntityCollectionLoader entityLoader;
 
     public CollectionEntityProxy(ColumnMeta columnMeta, Object id, EntityCollectionLoader entityLoader) {
@@ -23,13 +23,13 @@ public class CollectionEntityProxy implements InvocationHandler {
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         if (targetIsNull()) {
-            target = entityLoader.load(columnMeta.relationMeta().joinColumnType(), columnMeta.field(), id);
+            target = (List<T>) entityLoader.load(columnMeta.relationMeta().joinColumnType(), columnMeta.field(), id);
         }
         return method.invoke(target, args);
     }
 
-    public static List<?> createProxy(ColumnMeta columnMeta, Object id, EntityCollectionLoader entityLoader) {
-        return (List<?>) Proxy.newProxyInstance(
+    public static <T> List<T> createProxy(ColumnMeta columnMeta, Object id, EntityCollectionLoader entityLoader) {
+        return (List<T>) Proxy.newProxyInstance(
                         columnMeta.field().getType().getClassLoader(),
                         new Class[]{List.class},
                         new CollectionEntityProxy(columnMeta, id, entityLoader));
