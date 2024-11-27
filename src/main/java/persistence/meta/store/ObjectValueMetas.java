9e7484a0c8353957fc0ccbf825e7d775da08df52
hellonayeon <hellonykwon@gmail.com>
2024-11-27 21:05:10 +0900

    refactor: 엔티티 밸류 매핑 역할을 하는 ObjectValueMetas 추가

---
+++ src/main/java/persistence/meta/store/ObjectValueMetas.java
@@ -0,0 +1,16 @@
+package persistence.meta.store;
+
+import java.util.List;
+import persistence.meta.ColumnMeta;
+import persistence.meta.ColumnValueMeta;
+
+public class ObjectValueMetas {
+
+    public static List<Object> values(List<ColumnMeta> columnMetas, Object instance) {
+        return columnMetas.stream()
+                .map(columnMeta -> ColumnValueMeta.of(columnMeta.field(), instance))
+                .map(ColumnValueMeta::value)
+                .toList();
+    }
+
+}
